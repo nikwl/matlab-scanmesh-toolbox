ae8b879832e3f381213615050a7ad69c3b59e14c
CountingShe3p <nikolas.lamb@gmail.com>
2019-05-09 00:00:50 -0400

    added compute degree matrix

---
+++ helpers/compute_degreematrix.m
@@ -0,0 +1,87 @@
+function [degree] = compute_degreematrix(obj,varargin)
+% Returns a sparse matrix corresponding to edge incidence. Matrix has a row
+%   for each vertex and a column for each edge, and (v,e) = 1 iff v is
+%   incident upon edge e. 
+%
+% Inputs:
+% 	obj          -  object struct
+%   optional args:
+%      directed    -  assumes directed mesh (default)
+%      undirected  -  assumes undirected mesh
+% Outputs: 
+%   adjacencies  -  |v| x |e| sparse incidence matrix
+%
+% Local Dependancies:
+%   compute_edges
+%
+% Copyright (c) 2019 Nikolas Lamb
+%
+
+% Define flags and specifiers
+flags       = ["directed" "undirected"];    
+flagvals    = [1 0];
+specs       = [];
+specvals    = [];
+
+% Parse input
+if length(varargin) ~= 0
+    [flagvals,specvals] = parse_function_input(flags,flagvals,specs,specvals,varargin);
+end
+
+% Compute adjacency matrix
+if flagvals(1)
+    adjacencies = compute_adjacencymatrix(obj,'directed');
+else
+    adjacencies = compute_adjacencymatrix(obj,'undirected');
+end
+
+% Degree is the sum of the row of the adjacency matrix
+degree = sparse(1:length(adjacencies),1:length(adjacencies),full(sum(adjacencies,2)));
+
+end
+
+function [flagdef,specdef] = parse_function_input(flags,flagdef,specs,specdef,v)
+
+flagvals = zeros(1,length(flags));
+specvals = zeros(1,length(specs));
+
+i = 1;
+while i <= length(v)
+    % Get the location of the flag or specifier in the provided list
+    found = strcmpi(v{i},[flags,specs]);
+    
+    % Identifier not found
+    if ~any(found)
+        error('Unknown flag or specifier');
+    end
+    foundloc = find(found);
+    
+    % Identifier is a flag
+    if foundloc <= length(flags)
+        flagvals(foundloc) = 1;        
+    else % Identifier is a specifier
+        if i < length(v)
+            if ~any(strcmpi(v{i+1},[flags,specs]))
+                specvals(foundloc-length(flags)) = v{i+1};
+                i = i + 2;
+                continue
+            else
+                error('Specifier given with no value');
+            end
+        else
+            error('Specifier given with no value');
+        end
+    end
+    i = i + 1;
+end
+
+% Conditionals
+if flagvals(1) || flagvals(2) 
+    if flagvals(1) && flagvals(2) 
+        error('Cant be both directed and undirected');
+    end
+    flagdef(1) = flagvals(1);
+    flagdef(2) = flagvals(2);
+end
+
+end
